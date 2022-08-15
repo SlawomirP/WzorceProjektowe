c76c9b792f01928764cb44019474ed720b5e37b0
slawo <slawekpettke@gmail.com>
2022-08-15 09:03:30 +0200

    ---

---
+++ src/main/java/House.java
@@ -0,0 +1,14 @@
+public class House {
+
+    String adress;
+    Integer floorsNumber;
+    Integer doorsNumber;
+    Integer windowsNumber;
+
+    public House(String adress, Integer floorsNumber, Integer doorsNumber, Integer windowsNumber) {
+        this.adress = adress;
+        this.floorsNumber = floorsNumber;
+        this.doorsNumber = doorsNumber;
+        this.windowsNumber = windowsNumber;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        new House("ul dluga", 2, 3 , 12);
+    }
+}
