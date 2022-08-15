644b9ed4f3d1172d11b9bdd12e55c6967829a1fe
slawo <slawekpettke@gmail.com>
2022-08-15 16:08:05 +0200

    zamiana singletona na bezpieczniejsza wersje

--- src/main/java/singletonExpl/Logger.java
+++ src/main/java/singletonExpl/Logger.java
@@ -1,30 +1,17 @@
 package singletonExpl;
 
 public class Logger {
-    //TO ROZWIAZANIE JEST SREDNIO BEZPIECZNE, NP
-    //JEZELI ZOSTANIE WYWOLANY PRZEZ 2 WĄTKI
-    //UWAGA !!! pole prywatne klasy w ktorej jestesmy
     private static Logger instance;
 
-    //aby Loggera nie mozna bylo utworzyc w innym miejscu
-    //niz ta klasa to dodajemy
     private Logger() {
     }
 
-    //publiczny dostep i zwraca nam Loggera getter
     public static Logger getInstance() {
-        //jezeli instacja jest pusta to tworzy nowa i
-        //zwraca ta nowa jezeli jest cos w instancji
-        //to poprostu ja zwraca
         if (Logger.instance == null) {
-            instance = new Logger(); // to decyduje ze to wzorzec kreacyjny
+            instance = new Logger();
             return instance;
         } else {
             return instance;
         }
     }
-    //wywolujemy w mainie
-
-    //dalej jeszcze moze byc dodatkowa logika
-    //metody i inne ktore wywolujemy przez komende w mainie
 }
