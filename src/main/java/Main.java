ee7e1ffc9198797ee73f522582600710c9e9b83e
slawo <slawekpettke@gmail.com>
2022-08-15 10:27:55 +0200

    stworzenie buildera z automatu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-//        new House("ul dluga", 2, 3 , 12);
-        // new House .setAdress      .setFloors    . .setDoorsNumber .setWindowNumber
+//UTWORZENIE OBIEKTU PRZY POMOCY BUILDERA
 
-        //STWORZENIE OBIEKTU Z BUDOWNICZEGO
-//        House house = new HouseBuilder().createHouse()
-//                .setAdress("ul. Dluga")
-//                .setDoorsNumber(3)
-//                .setWindowsNumber(10)
-//                .createHouse();
-//    }
+        House house = new House.HouseBuilder()
+                .setAdress("ul. Dluga")
+                .setDoorsNumber(3)
+                .setWindowsNumber(10)
+                .build();
+    }
 }
