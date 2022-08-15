7f523fc716057e6a8a79d08341f1cfd2c6ceccc6
slawo <slawekpettke@gmail.com>
2022-08-15 09:42:42 +0200

    ---

--- src/main/java/House.java
+++ src/main/java/House.java
@@ -1,9 +1,42 @@
 public class House {
 
-    String adress;
-    Integer floorsNumber;
-    Integer doorsNumber;
-    Integer windowsNumber;
+    private String adress;
+    private Integer floorsNumber;
+    private Integer doorsNumber;
+    private Integer windowsNumber;
+
+    public House() {
+        //USUWAMY TO Z KONSTRUKTORA I ZAMIAST TEGO DAJEMY SETTERY DO POL
+//        this.adress = adress;
+//        this.floorsNumber = floorsNumber;
+//        this.doorsNumber = doorsNumber;
+//        this.windowsNumber = windowsNumber;
+    }
+
+    // W TYCH SETERACH DAJEMY ZAMIAST VOID TO TYP zwracany HOUSE
+    // A NIZEJ POPROSTU RETURN THIS
+    public House setAdress(String adress) {
+        return this;
+    }
+
+    public House setFloorsNumber(Integer floorsNumber) {
+        return this;
+    }
+
+    public House setDoorsNumber(Integer doorsNumber) {
+        return this;
+    }
+
+    public House setWindowsNumber(Integer windowsNumber) {
+        return this;
+    }
+
+    //NAPISANIE METODY WYTWORZENIA, z polami ktore ustawiamy metodami
+    public House createHouse() {
+        return new House(adress, floorsNumber, doorsNumber, windowsNumber);
+    }
+
+    //STWORZENIE BRAKUJACEGO KONSTRUKTORA
 
     public House(String adress, Integer floorsNumber, Integer doorsNumber, Integer windowsNumber) {
         this.adress = adress;
@@ -11,4 +44,9 @@ public class House {
         this.doorsNumber = doorsNumber;
         this.windowsNumber = windowsNumber;
     }
+
+    // POTEM W MAINIE TWORZYMY OBIEKT HOUSE
 }
+
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        new House("ul dluga", 2, 3 , 12);
+//        new House("ul dluga", 2, 3 , 12);
+        // new House .setAdress      .setFloors    . .setDoorsNumber .setWindowNumber
+
+        //STWORZENIE OBIEKTU Z BUDOWNICZEGO
+        House house = new House()
+                .setAdress("ul. Dluga")
+                .setDoorsNumber(3)
+                .setWindowsNumber(10)
+                .createHouse();
     }
 }
