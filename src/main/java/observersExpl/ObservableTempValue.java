0c9e74828f0f255511a161a48b2b8ec256ceeac8
slawo <slawekpettke@gmail.com>
2022-08-16 09:14:15 +0200

    Obserwator z API javy

--- src/main/java/observersExpl/ObservableTempValue.java
+++ src/main/java/observersExpl/ObservableTempValue.java
@@ -10,14 +10,16 @@ public class ObservableTempValue extends Observable {
     //wklejamy wnetrze z klasy anonimowej, dzieki temu
     //ze mamy to w naszej metodzie mozna zmienic nazwe na
     //bardziej dla nas czytelna i mozna zmienic wnetrze
+    //jezeli wiadomo ze to temp to mozna dac int
     public void setValue(int newTemp) { //trzeba tez zmienic nazwe metody do obserwacji
-                                        //o ile sama sie nie zmienila shift+F6
-        if(newTemp != oldTemp){
-            super.setChanged(); // sprzwdzenie zmiany
+        //o ile sama sie nie zmienila shift+F6
+        if (newTemp != oldTemp) {
+            super.setChanged(); // sprzwdzenie zmiany, to flaga sterująca, wszyscy usłysza
             super.notifyObservers(newTemp); // powiadomienie
             oldTemp = newTemp; // aktualizuje temp
         }
-            }
+    }
+
     //geter do starej temp
     public int getOldTemp() {
         return oldTemp;
