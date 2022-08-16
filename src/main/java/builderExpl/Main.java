4c0f317e0858caad66de263a2e659aea1a4b9bd3
slawo <slawekpettke@gmail.com>
2022-08-16 20:38:08 +0200

    wzorzec dekorator implementacja

--- src/main/java/builderExpl/Main.java
+++ src/main/java/builderExpl/Main.java
@@ -42,8 +42,19 @@ public class Main {
         otMike.goToClient();
 
         //WZORZEC DEKORATOR
+
+        mike.setSalary(10000);
+
         System.out.println("zarobki " + mike.getSalary());
         System.out.println("zarobki " + new FreqBonus(new DeadlineBonus(new SpecialBonus(mike))).getSalary());
+
+        Payable employee = mike;
+        if(mike.getSalary() > 8000){
+            employee = new SpecialBonus(employee);
+        }
+        if(mike.travelStrategy instanceof BikeTravelStrategy){
+            employee = new FreqBonus(employee);
+        }
 
 
 
